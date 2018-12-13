095b343c4058b7a6dc6f2bba1b889db2356ac7b4
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2018-12-13 18:04:15 -0500

    Initial Commit

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by DANIEL OREILLY on 12/13/18.
+//  Copyright © 2018 DANIEL OREILLY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
